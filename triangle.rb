403dc5be17e57f303eb2ff63756f45f5a5f38667
Daiki Tanoguchi <sunsetocean85@gmail.com>
2012-06-05 22:09:24 +0900

    modify scalene triangle

--- triangle.rb
+++ triangle.rb
@@ -6,6 +6,12 @@ class Triangle
   def is_isoscales_triangle?
    preCondition? && (@a == @b || @b == @c || @c == @a)
   end
+  def is_triangle?
+    preCondition? && (a < b + c) && (b < a + c) && ( c < a + b) 
+  end
+  def is_scalene_triangle?
+    return is_triangle? && !is_isoscales_triangle?
+  end
   def preCondition?
     unless [@a,@b,@c].find{|n| n.is_a?(Numeric) && n > 0} 
       false
