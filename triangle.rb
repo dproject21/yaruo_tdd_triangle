38095ca6d18c11ffb19662d82a048ad4f0bc66cf
Daiki Tanoguchi <sunsetocean85@gmail.com>
2012-07-09 21:13:35 +0900

    extract nil condition

--- triangle.rb
+++ triangle.rb
@@ -1,26 +1,31 @@
 class Triangle
   attr_accessor :a, :b, :c
   def is_equilteral_triangle?
-    preCondition? && @a == @b && @b== @c && @c == @a
+   not_nil? && preCondition? && @a == @b && @b== @c && @c == @a
   end
   def is_isoscales_triangle?
-   preCondition? && (@a == @b || @b == @c || @c == @a)
+   not_nil? && preCondition? && (@a == @b || @b == @c || @c == @a)
   end
   def is_triangle?
-    preCondition? && (a < b + c) && (b < a + c) && ( c < a + b) 
+   not_nil? && preCondition? && (a < b + c) && (b < a + c) && ( c < a + b) 
   end
   def is_scalene_triangle?
     return is_triangle? && !is_isoscales_triangle?
   end
   def preCondition?
-    if (@a.nil? || @b.nil? || @c.nil?) 
-      return false
-    end
-    
     unless [@a,@b,@c].find{|n| n.is_a?(Numeric) && n > 0} 
       false
     else
       true
     end
   end
+
+  def not_nil?
+    if (@a.nil? || @b.nil? || @c.nil?) 
+      false
+    else
+      true
+    end
+  end
+
 end
